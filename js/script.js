02aec5c74effcf6d3f37bf12730d7ebd7b92be10
batsnuff <cryptobts7@gmail.com>
2024-01-31 21:34:41 +0100

    added business card, first commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+function night(){
+ let night = document.querySelector(".main");
+ night.classList.toggle("night");
+ let background = document.querySelector("body");
+ background.classList.toggle("change");
+}
+
+function openCodedCV() {
+    window.open('https://batsnuff.github.io/my-coded-CV/', '_blank')
+    console.log("hej")
+  }
+
+function openLinkedin() {
+  window.open('https://www.linkedin.com/in/miłosz-szczepaniak-1708b9178/', '_blank');
+}
+
+function openGitHub() {
+    window.open('https://github.com/batsnuff', '_blank');
+}
+
+function openInstagram() {
+    window.open('https://www.instagram.com/batsnuff/', '_blank')
+}
+
+function openTelegram() {
+    window.open('https://t.me/batsnuff', '_blank')
+}
